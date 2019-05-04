40b351ba3d297775384b2054d35d6a7725874906
FAZH <fazepher@gmail.com>
2019-05-03 22:05:08 -0500

    Avance AED electoral

--- SCRIPTS_R/05_AED_ELECTORAL.R
+++ SCRIPTS_R/05_AED_ELECTORAL.R
@@ -85,7 +85,7 @@ genera_geofacet_distr_votos <- function(elec, familia){
     geofacet::facet_geo(~COD_REG, grid = filter(fr_anc_reg,col<=6), label = "name") + 
     scale_fill_manual(values = color) + 
     #scale_color_manual(values = color) + 
-    scale_alpha_continuous(range = c(0.4,1)) + 
+    scale_alpha_continuous(range = c(0.3,1)) + 
     scale_x_continuous(breaks = seq(0,1,0.2),labels = paste(seq(0,100,20),"%",sep=""),trans = "log1p") + 
     scale_y_continuous(trans = "log1p") + 
     labs(title = paste(familia,"en las",elec,sep = " "), 
@@ -101,11 +101,118 @@ genera_geofacet_distr_votos <- function(elec, familia){
   
 }
 
+
+geofacet_corr_leg_pres <- function(aaaa,familia_base){
+  
+  datos_familia_base <- datos_electorales_completos %>% 
+    separate(ELECCION,c("Tipo","Año")) %>% 
+    filter(Año == as.character(aaaa), Tipo == "Legislativas", FAMILIA == familia_base) %>% 
+    transmute(Año,CODGEO,PCT_LEGIS_FAM_BASE=PCT_VOTOS_BR)
+  
+  datos_otras_familias <- datos_electorales_completos %>% 
+    separate(ELECCION,c("Tipo","Año")) %>% 
+    filter(Año == as.character(aaaa), Tipo == "Presidenciales") %>% 
+    select(CODGEO,FAMILIA,PCT_VOTOS_BR) %>% 
+    spread(FAMILIA,PCT_VOTOS_BR)
+  
+  datos_unidos <- left_join(datos_familia_base,datos_otras_familias)
+  
+  left_join(datos_familia_base,datos_otras_familias) %>% 
+    inner_join(COMUNAS_2007) %>% 
+    group_by(COD_DPTO) %>% 
+    sample_frac(0.25) %>% 
+    gather(FAMILIA,PCT_VOT_BR_PRES,one_of(familias_politicas)) %>% 
+    filter(FAMILIA == "Izquierda") %>% 
+    {ggplot(.,aes(x=PCT_VOT_BR_PRES,y=log(PCT_LEGIS_FAM_BASE/(1-PCT_LEGIS_FAM_BASE)),color=FAMILIA)) + 
+        geom_point(alpha = 0.4) + 
+        geofacet::facet_geo(~COD_REG, grid = filter(fr_anc_reg,col<=6), label = "name",scales = "free") + 
+        scale_color_manual(values = set_names(paleta_tesis_fn$COLOR,paleta_tesis_fn$FAMILIA))} %>% 
+    ggsave(plot = ., filename = "Probando.pdf",device = cairo_pdf,width = 25,height = 25)
+  
+}
+
+
+geofacet_pct_votos_dpto <- function(elec,familia){
+  
+  lim_y <- datos_electorales_completos %>% 
+    filter(FAMILIA == familia) %>% 
+    top_n(1,PCT_VOTOS_BR) %>% 
+    extract2("PCT_VOTOS_BR") %>% 
+    unique
+  
+  color <- filter(paleta_tesis_fn, FAMILIA == familia) %>% 
+    extract2("COLOR")
+  
+  pct_comuna_sub <- datos_electorales_completos %>% 
+    filter(FAMILIA == familia, ELECCION == elec) %>% 
+    ggplot(aes(x="Metrópoli entera",y=PCT_VOTOS_BR,color=FAMILIA)) + 
+    geom_hline(aes(yintercept = median(PCT_VOTOS_BR)),color="gray20") + 
+    geom_hline(aes(yintercept = quantile(PCT_VOTOS_BR,0.25)),color = "gray50") + 
+    geom_hline(aes(yintercept = quantile(PCT_VOTOS_BR,0.75)),color = "gray50") + 
+    geom_violin(fill = "transparent") +
+    scale_color_manual(values = color) + 
+    scale_y_continuous(breaks = seq(0,1,0.25),labels = paste(seq(0,100,25),"%"),  
+                       limits = c(0,lim_y), trans = "log1p") + 
+    scale_x_discrete(position = "top") +
+    theme_minimal() + 
+    theme(legend.position = "none",
+          panel.grid = element_blank(),
+          axis.title = element_blank(),
+          axis.text.x = element_text(margin = margin(30,0,10,0), size = 10),
+          axis.text.y = element_text(size = 15))
+  
+  pct_comuna_dpto <- datos_electorales_completos %>% 
+    filter(FAMILIA == familia, ELECCION == elec) %>% 
+    inner_join(COMUNAS_2007) %>% 
+    mutate(MEDIANA_NAL = median(PCT_VOTOS_BR)) %>% 
+    group_by(COD_REG) %>% 
+    mutate(MEDIANA_REG = median(PCT_VOTOS_BR)) %>% 
+    ungroup %>% 
+    mutate(NOM_REG = reorder(NOM_REG,MEDIANA_REG)) %>% 
+    group_by(COD_DPTO) %>% 
+    mutate(MEDIANA_DPTO = median(PCT_VOTOS_BR)) %>% 
+    ungroup %>% 
+    mutate(COD_DPTO = reorder(COD_DPTO,MEDIANA_DPTO)) %>% 
+    ggplot(aes(x = COD_DPTO, y = PCT_VOTOS_BR, fill = FAMILIA)) + 
+    geom_hline(aes(yintercept = median(PCT_VOTOS_BR)),color="gray20") + 
+    geom_hline(aes(yintercept = quantile(PCT_VOTOS_BR,0.25)),color = "gray50") + 
+    geom_hline(aes(yintercept = quantile(PCT_VOTOS_BR,0.75)),color = "gray50") + 
+    geom_violin(aes(alpha = MEDIANA_DPTO/MEDIANA_NAL)) + 
+    geofacet::facet_geo(~COD_REG, grid = filter(fr_anc_reg,col<=6), label = "name", scales = "free_x") + 
+    scale_fill_manual(values = color) + 
+    scale_alpha_continuous(range = c(0.3,1)) + 
+    scale_y_continuous(breaks = seq(0,1,0.25),labels = paste(seq(0,100,25),"%"), 
+                       limits = c(0,lim_y), trans = "log1p") + 
+    theme_minimal() + 
+    labs(title = paste(familia,"en las",elec,sep = " "), 
+         subtitle = "% de votos por comuna por departamento") + 
+    theme(legend.position = "none", 
+          panel.grid = element_blank(), 
+          axis.title.y = element_blank(),
+          axis.text.x = element_text(margin = margin(30,0,10,0), size = 10),
+          axis.ticks.x = element_blank(),
+          axis.title.x = element_blank(),
+          axis.text.y = element_blank(),#element_text(size = 15),
+          strip.text.x = element_text(size = 15, margin = margin(10,0,0,0)),
+          plot.title = element_text(size = 30, hjust = 0.5),
+          plot.subtitle = element_text(margin = margin(5,0,25,0), size = 20, hjust = 0.5))
+  
+  ggplot(tibble(x=0:1,y=0:1),aes(x,y)) +
+    theme_void() +
+    annotation_custom(grob = ggplotGrob(pct_comuna_dpto),
+                      xmin = 0,xmax = 1,ymin = 0,ymax = 1) +
+    annotation_custom(grob = ggplotGrob(pct_comuna_sub),
+                      xmin = 0.82,xmax = 0.92,ymin = 0.8,ymax = 0.95) %>%
+    return(.)
+  
+}
+
+
 geofacets_distr_por_reg <-list(elección = unique(datos_electorales_completos$ELECCION),
                                familia = familias_politicas) %>% 
   cross_df %>% 
   filter(familia == "FN") %>% 
-  mutate(Archivo = paste("AED/ELECTORALES/Geofacet_Distr_por_Reg_",
+  mutate(Archivo = paste("AED/ELECTORALES/Geofacet_Reg/Geofacet_Distr_por_Reg_",
                          str_remove_all(elección,"(residenciales 20)|(egislativas 20)"),
                          "_",
                          str_replace_all(familia, " ", "_"),
@@ -113,6 +220,19 @@ geofacets_distr_por_reg <-list(elección = unique(datos_electorales_completos$ELE
                          sep = "")) %>% 
   pmap(~genera_geofacet_distr_votos(..1,..2) %T>% 
          ggsave(plot = ., width = 18, height = 15, device = cairo_pdf,
+                filename =..3))
+
+geofacets_distr_por_dpto <- list(elección = unique(datos_electorales_completos$ELECCION),
+                                 familia = familias_politicas) %>% 
+  cross_df %>% 
+  mutate(Archivo = paste("AED/ELECTORALES/Geofacet_Dpto/Geofacet_Distr_por_Dpto_",
+                         str_remove_all(elección,"(residenciales 20)|(egislativas 20)"),
+                         "_",
+                         str_replace_all(familia, " ", "_"),
+                         ".pdf",
+                         sep = "")) %>% 
+  pmap(~geofacet_pct_votos_dpto(..1,..2) %T>% 
+         ggsave(plot = ., width = 25, height = 25, device = cairo_pdf,
                 filename =..3))
                                  
 
